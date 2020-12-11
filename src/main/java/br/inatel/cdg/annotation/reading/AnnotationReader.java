935bbce5a2b25a5a46a73e7e2ead80d7dc30b878
VanessaSwerts <vanessaswerts@gec.inatel.br>
2020-12-11 08:19:20 -0300

    create project

---
+++ src/main/java/br/inatel/cdg/annotation/reading/AnnotationReader.java
@@ -0,0 +1,19 @@
+package br.inatel.cdg.annotation.reading;
+
+//Nessa classe você deverá implementar a sua solução
+//Adicionalmente você também deverá criar
+//a anotação NumberToSum como parte da solução
+
+public class AnnotationReader<T> {
+
+	public AnnotationReader(Class<?> clazz) {
+	}
+
+	public void readAnnotations() {
+	}
+	
+	public int getTotalSum() {
+		return 0;
+	}
+
+}
